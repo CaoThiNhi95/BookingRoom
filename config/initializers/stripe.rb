c3033b5445233a0b409a99917eb4188a0a048f9c
Macbook <macbook@Macbooks-MacBook-Pro-2.local>
2021-02-09 00:18:53 +0900

    Payment Form + Payment Controller

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  :publisable_key => ENV["STRIPE_PUBLIC_KEY"],
-  :secret_key => ENV["STRIPE_SECRET_KEY"]
+  publishable_key: ENV['STRIPE_PUBLIC_KEY'],
+  secret_key: ENV['STRIPE_SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
